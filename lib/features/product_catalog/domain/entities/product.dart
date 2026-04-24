f3a1517547db6b396e4506c4bbd068b3730e2a37
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2026-04-24 03:08:57 -0500

    feat: :construction: Product entity was created.

---
+++ lib/features/product_catalog/domain/entities/product.dart
@@ -0,0 +1,19 @@
+class Product {
+  final int id;
+  final String title;
+  final String beauty;
+  final double price;
+  final double raiting;
+  final String brand;
+  final List<String> image;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.beauty,
+    required this.price,
+    required this.raiting,
+    required this.brand,
+    required this.image,
+  });
+}
