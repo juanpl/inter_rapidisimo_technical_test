631e8a3fbedac00f426cb1d1f216d40c1533f648
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2026-04-26 11:05:43 -0500

    feat: :rocket: The missing dependency injections were performed
    The missing dependency injections were performed to complete the development of the domain layer.

--- lib/features/product_catalog/product_catalog_injection_container.dart
+++ lib/features/product_catalog/product_catalog_injection_container.dart
@@ -1,9 +1,24 @@
 import 'package:get_it/get_it.dart';
+import 'package:inter_rapidisimo_technical_test/features/product_catalog/data/datasources/product_catalog_datasource.dart';
+import 'package:inter_rapidisimo_technical_test/features/product_catalog/data/repository_impl/product_catalog_repository_impl.dart';
+import 'package:inter_rapidisimo_technical_test/features/product_catalog/domain/repositories/product_catalog_repository.dart';
 import 'package:inter_rapidisimo_technical_test/features/product_catalog/domain/use_cases/get_product_catalog_use_case.dart';
 
 final getIt = GetIt.instance;
 
 void productCatalogInjectionContainer(GetIt getIt) {
+  if (!getIt.isRegistered<ProductCatalogDatasource>()) {
+    getIt.registerSingleton<ProductCatalogDatasource>(
+      ProductCatalogDatasource(),
+    );
+  }
+
+  if (!getIt.isRegistered<ProductCatalogRepository>()) {
+    getIt.registerSingleton<ProductCatalogRepository>(
+      ProductCatalogRepositoryImpl(),
+    );
+  }
+
   if (!getIt.isRegistered<GetProductCatalogUseCase>()) {
     getIt.registerSingleton<GetProductCatalogUseCase>(
       GetProductCatalogUseCase(),
@@ -12,5 +27,7 @@ void productCatalogInjectionContainer(GetIt getIt) {
 }
 
 void unregisterProductCatalogInjectionContainer() {
+  GetIt.I.unregister<GetProductCatalogUseCase>();
+  GetIt.I.unregister<ProductCatalogRepository>();
   GetIt.I.unregister<GetProductCatalogUseCase>();
 }
